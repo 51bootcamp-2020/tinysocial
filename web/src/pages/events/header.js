2cf05f5d067d4cfa3dcfdc7cf1c1742d0d196d21
Sang-geon Yun <c0510gy@protonmail.ch>
2020-01-09 10:29:46 -0800

    little update on events.js

---
+++ web/src/pages/events/header.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Events extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+Events.propTypes = {};
+
+export default Events;
