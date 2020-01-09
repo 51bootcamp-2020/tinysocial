42f7e2dda9fe0ecb5add71810d62cf576917e475
SiHyun Lee <lsh9034@kookmin.ac.kr>
2020-01-09 11:34:24 -0800

    issue #5

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Landing from './pages/landing';
+import LoginForm from "./components/login-form";
 
 function App() {
   return (
       <div>
         Main
+          <LoginForm/>
         <Landing/>
       </div>
   );
--- web/src/components/login-form.js
+++ web/src/components/login-form.js
@@ -1,11 +1,33 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import {GoogleLogin} from 'react-google-login';
+import { clientID } from './utils.js'
 
 class LoginForm extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    // Google login success callback function
+    responseGoogle =  (res) => {
+        console.log(res);
+    };
+
+    // Google login fail callback function
+    responseFail = (err) => {
+        console.error(err);
+    };
   render() {
+
     return (
         <div>
-
+            {/* Google Login Button */}
+            <GoogleLogin
+                onSuccess={this.responseGoogle}
+                onFailure={this.responseFail}
+                clientId={clientID} // our Client ID
+            />
         </div>
     );
   }
