e5ed0bc87e781abb04768fe0e2375c54fc067856
Arin Kwak <rev1c0sm0s@gmail.com>
2020-01-09 23:12:58 -0800

    scaffolding: Add sign-up/sign-in logic.

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -1,8 +1,57 @@
 module.exports.Mutation = {
-  signInWithGoogle: async (_, {googleId}) => {
+  signInWithGoogle: async (_, {googleId}, {dataSources}) => {
+    const user = await dataSources.mainAPI.findOneUser({googleId});
+    if (user === null) {
+      return {
+        success: false,
+        message: 'User not found. You have to sign up first.',
+        token: null,
+        user: null,
+      };
+    }
+
+    // TODO(lsh9034): Implement session logic.
+    //  Create session and return sessionId to client.
+
+    return {
+      success: true,
+      message: 'Success',
+      token: 'We have to implement this',
+      user: user.get({plain: true}),
+    };
   },
-  signUpWithGoogle: async (_, {googleId, email, firstName, lastName}) => {
-  },
-  logout: () => {
+
+  signUpWithGoogle:
+      async (
+          _, {googleId, email, firstName, lastName, profileImgUrl},
+          {dataSources}) => {
+        const user = await dataSources.mainAPI.findOrCreateUser({googleId},
+            {
+              email,
+              firstName,
+              lastName,
+              profileImgUrl: profileImgUrl ? profileImgUrl : '',
+            });
+        if (user === null) {
+          return {
+            success: false,
+            message: 'Fail to create the user. Please try again later',
+            token: null,
+            user: null,
+          };
+        }
+
+        // TODO(lsh9034): Implement session logic.
+        //  Create session and return sessionId to client.
+
+        return {
+          success: true,
+          message: 'Success',
+          token: 'We have to implement this',
+          user: user.get({plain: true}),
+        };
+      },
+
+  logout: async () => {
   },
 };
