8205194d2e3cbbb1ac6b9a2f26306b76b1bc4d7e
Sang-geon Yun <c0510gy@protonmail.ch>
2020-01-10 10:59:52 -0800

    refactor: refactoring eventlist.js

--- web/src/pages/eventslist.js
+++ web/src/pages/eventslist.js
@@ -1,54 +1,54 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Tabs from "react-bootstrap/Tabs";
-import Tab from "react-bootstrap/Tab";
+
 import Cards from "../components/eventslist-component/cards";
 
-import {Container, Row, Col} from 'react-bootstrap';
-
-import Button from "react-bootstrap/Button";
-import CardGroup from "react-bootstrap/CardGroup";
-
-import CardColumns from "react-bootstrap/CardColumns";
+import {Container, Row, Col, Tabs, Tab} from 'react-bootstrap';
 
 class Eventslist extends Component {
-  render() {
+    constructor(props) {
+        super(props);
 
-    return(
-        <div>
-          Here is the list of events
-            <Container>
-                <Row className="justify-content-md-center">
-                    <Col md="auto">
-                        <Tabs defaultActiveKey="all">
-                            <Tab eventKey="all" title="All" class="center-block">
-                                <Cards area={"all"} />
-                            </Tab>
-                            <Tab eventKey="sanfran" title="San Francisco">
-                                {/*<p>this is San Francisco!!!!!</p>*/}
-                                <Cards area={"sanfran"} />
+        const places = [
+            {eventKey: "all", title: "All"},
+            {eventKey: "sanfran", title: "San Francisco"},
+            {eventKey: "sanmateo", title: "San Mateo"},
+            {eventKey: "mountainview", title: "Mountain View"},
+        ];
 
-                            </Tab>
-                            <Tab eventKey="sanmateo" title="San Mateo">
-                                {/*<p>this is San Mateo</p>*/}
-                                <Cards area={"sanmateo"} />
+        this.state = {
+            places: places
+        };
+    }
 
-                            </Tab>
-                            <Tab eventKey="mountainview" title="Mountain View">
-                                {/*<p>this is Mountain View</p>*/}
-                                <Cards area={"mountainview"} />
-                            </Tab>
-                        </Tabs>
-                    </Col>
-                </Row>
-            </Container>
+    TabComponents = () => {
+        const components = [];
 
-        </div>
+        for(let j = 0; j < this.state.places.length; j++){
+            components.push(<Tab eventKey={this.state.places[j].eventKey} title={this.state.places[j].title} class="center-block">
+                                <Cards area={this.state.places[j].eventKey}/>
+                            </Tab>);
+        }
 
-    )
+        return components;
+    };
 
-  }
+    render() {
+        return (
+            <div>
+                <br/>
+                <Container>
+                    <Row className="justify-content-md-center">
+                        <Col md="auto">
+                            <Tabs defaultActiveKey={this.state.places[0].eventKey}>
+                                {this.TabComponents()}
+                            </Tabs>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        )
+    }
 }
 
 Eventslist.propTypes = {};
