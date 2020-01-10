532b5790c1ba061c22b1df1928847ccb710fb0de
Hyejin Lee <hub0913@kookmin.ac.kr>
2020-01-10 11:38:42 -0800

    "feat : Implement sign in query"
    "query sending user information to server"

--- web/src/components/login-form.js
+++ web/src/components/login-form.js
@@ -1,35 +1,59 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {GoogleLogin} from 'react-google-login';
-import { clientID } from './utils.js'
+import {clientID} from './utils.js';
+import {gql} from 'apollo-boost';
+
+/*query sending user Information to server*/
+const SIGNIN_QUERY = gql`
+        mutation ($googleId: String!){
+          signInWithGoogle(google: $googleId){
+            success
+            message
+            token
+            user
+          }
+        }`;
 
 class LoginForm extends Component {
 
-    constructor(props) {
-        super(props);
-    }
-
-    // Google login success callback function
-    responseGoogle =  (res) => {
-        console.log(res);
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAuthenticated: false,
+      isMember: false,
     };
+  }
 
-    // Google login fail callback function
-    responseFail = (err) => {
-        console.error(err);
-    };
+  // Google login success callback function
+  responseGoogle = async (res) => {
+    const userInfo = res.profileObj;
+
+    // send userInfo data to server
+    const {data} = await this.props.client.query({
+      mutate: {SIGNIN_QUERY},
+      variables: {
+        googleId: userInfo.googleId,
+      },
+    });
+
+  };
+
+  // Google login fail callback function
+  responseFail = (err) => {
+    console.error(err);
+  };
+
   render() {
-
     return (
         <div>
-            {/* Google Login Button */}
-            <GoogleLogin
-                onSuccess={this.responseGoogle}
-                onFailure={this.responseFail}
-                clientId={clientID} // our Client ID
-            />
-        </div>
-    );
+          {/* Google Login Button */}
+          <GoogleLogin
+              onSuccess={this.responseGoogle}
+              onFailure={this.responseFail}
+              clientId={clientID} // our Client ID
+          />
+        </div>);
   }
 }
 
