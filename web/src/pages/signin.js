696a1be0c21c0c3b633a5ba966b431958482344d
mskwon1 <mskwon@kookmin.ac.kr>
2020-01-09 22:00:47 -0800

    feat: install react-router-dom and add routes from App.js
    install react-router-dom
    add routes /, /signin, /signup
    include temporary style codes for the navigation bar
    add pages/signin.js blank page

--- web/src/pages/signin.js
+++ web/src/pages/signin.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+<<<<<<< HEAD
 import LoginForm from '../components/login-form'
 
 // TODO(Hyejin): Implement signin page using material UI
@@ -12,4 +13,20 @@ class Signin extends Component{
     };
 }
 
+=======
+import PropTypes from 'prop-types';
+
+class Signin extends Component {
+  render() {
+    return (
+        <div>
+
+        </div>
+    );
+  }
+}
+
+Signin.propTypes = {};
+
+>>>>>>> feat: install react-router-dom and add routes from App.js
 export default Signin;
