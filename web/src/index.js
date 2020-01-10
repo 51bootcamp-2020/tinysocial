d29703da47178976d6a87f400194ef8f4c465f89
Arin Kwak <rev1c0sm0s@gmail.com>
2020-01-10 15:09:21 -0800

    scaffolding: Add react-bootstrap.

--- web/src/index.js
+++ web/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './service-worker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 
