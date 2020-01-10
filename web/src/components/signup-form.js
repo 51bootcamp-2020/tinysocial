ccbf369de52916e129ab8d9efab500f934b5ceff
SeongJaeSong <tjdkskgnal61@gmail.com>
2020-01-09 16:15:01 -0800

    Create register form
    When user click a google button, get user info and fulfill the register
    form.

--- web/src/components/signup-form.js
+++ web/src/components/signup-form.js
@@ -7,27 +7,62 @@ class SignupForm extends Component {
     super(props);
     this.state = {
       id: "",
-      name: "",
+      firstName: "",
+      lastName: "",
+      email: "",
       provider: ""
     };
   }
 
   responseGoogle = res => {
-    console.log(res.profileObj.googleId);
+    console.log(res);
+    this.setState({
+      id: res.profileObj.googleId,
+      firstName: res.profileObj.givenName,
+      lastName: res.profileObj.familyName,
+      email: res.profileObj.email,
+      provider: res.tokenObj.idpId
+    });
   };
 
   responseFail = err => {
     console.error(err);
   };
 
+  handleClick = () => {}; // ToDo: When ""Submit button" clicked, send user info to backend.
   render() {
+    const googleAuthAPIClientID =
+      "420478568442-ltur9qc3g9uam6f166k1pgsa7f2evl5e.apps.googleusercontent.com";
     return (
-      <GoogleLogin
-        clientId="420478568442-ltur9qc3g9uam6f166k1pgsa7f2evl5e.apps.googleusercontent.com"
-        buttonText="Google Log-in"
-        onSuccess={this.responseGoogle}
-        onFailure={this.responseFail}
-      />
+      <div>
+        <ul>
+          <li>Email: </li>
+          <input type="text" defaultValue={this.state.email} disabled></input>
+          <li>FirstName: </li>
+          <input
+            type="text"
+            defaultValue={this.state.firstName}
+            disabled
+          ></input>
+          <li>LastName: </li>
+          <input
+            type="text"
+            defaultValue={this.state.lastName}
+            disabled
+          ></input>
+        </ul>
+        <input
+          type="submit"
+          value="Register"
+          onClick={this.handleClick}
+        ></input>
+        <GoogleLogin
+          clientId={googleAuthAPIClientID}
+          buttonText="Google Log-in"
+          onSuccess={this.responseGoogle}
+          onFailure={this.responseFail}
+        />
+      </div>
     );
   }
 }
