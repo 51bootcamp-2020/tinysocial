52d93c900e589be5b7f9716735dedceb16bbf3e9
mskwon1 <mskwon@kookmin.ac.kr>
2020-01-11 20:55:35 -0800

    feat: change mutation logic using mutation component

--- web/src/components/signup-form.js
+++ web/src/components/signup-form.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import {GoogleLogin} from "react-google-login";
-import {gql} from 'apollo-boost'
-import {Mutation} from 'react-apollo'
+import {gql} from 'apollo-boost';
+import {Mutation} from 'react-apollo';
+import {withRouter} from 'react-router-dom'
 
 class SignupForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      id: "",
-      firstName: "",
-      lastName: "",
-      email: "",
-      provider: ""
+      id: '',
+      firstName: '',
+      lastName: '',
+      email: '',
+      // TODO : maybe we don't need this?
+      provider: '',
     };
   }
 
@@ -28,26 +30,19 @@ class SignupForm extends Component {
   };
 
   responseFail = err => {
-    // handle about google log-in failure
+    // TODO : handle google log-in failure
     console.error(err);
   };
 
-  handleClick = () => {
+  render() {
+    // TODO : hide this somehow
+    const googleAuthAPIClientID =
+      "420478568442-ltur9qc3g9uam6f166k1pgsa7f2evl5e.apps.googleusercontent.com";
+
+    // graphql mutation statement for signup
     const SIGNUP_MUTATION = gql`
-      mutation SignUp(
-        $googleId: String!
-        $email: String!
-        $firstName: String!
-        $lastName: String!
-        $profileImgUrl: String
-      ) {
-        signUpWithGoogle(
-          googleId: $googleId
-          email: $email
-          firstName: $firstName
-          lastName: $lastName
-          profileImgUrl: $profileImgUrl
-        ) {
+    mutation SignUp($googleId: String!, $email: String!, $firstName: String!, $lastName: String!, $profileImgUrl: String) {
+      signUpWithGoogle(googleId: $googleId, email: $email, firstName: $firstName, lastName: $lastName, profileImgUrl: $profileImgUrl) {
           success
           message
           token
@@ -56,68 +51,63 @@ class SignupForm extends Component {
             firstName
             email
           }
-        }
+        } 
       }
     `;
-    
-    // TODO : delete this part and Mutate using Mutate tag and state
-    const client = this.props.client
+
+    // receive state values for mutation use
     const {id, email, firstName, lastName} = this.state
 
-    client.mutate({
-      mutation: SIGNUP_MUTATION,
-      variables: {
-        googleId: id,
-        email: email,
-        firstName: firstName,
-        lastName: lastName,
-      }
-    }).then((data) => {
-      const { success, message, token, user } = data.data.signUpWithGoogle
-      if (success) { // sign up success
-        // TODO : redirect to the "/" page(landing page) with authentication info
-      } else {  // sign up failure
-        // TODO : show alert message and refresh the page, 
-        // this might have to be implemented in the other way
-        window.alert('Sign Up Failed!')
-        window.location.reload(false)
-      }
-    })
-  };
-
-  render() {
-    // TODO : hide this for a secret
-    const googleAuthAPIClientID =
-      "420478568442-ltur9qc3g9uam6f166k1pgsa7f2evl5e.apps.googleusercontent.com";
     return (
       <div>
+        {/* form, uneditable for now */}
         <ul>
           <li>Email: </li>
           <input type="text" defaultValue={this.state.email} disabled></input>
           <li>FirstName: </li>
-          <input
-            type="text"
-            defaultValue={this.state.firstName}
-            disabled
-          ></input>
+          <input type="text" defaultValue={this.state.firstName} disabled></input>
           <li>LastName: </li>
-          <input
-            type="text"
-            defaultValue={this.state.lastName}
-            disabled
-          ></input>
+          <input type="text" defaultValue={this.state.lastName} disabled></input>
         </ul>
-        <input
-          type="submit"
-          value="Register"
-          onClick={this.handleClick}
-        ></input>
+        {/* register button, sends a mutation to the server */}
+        <Mutation 
+          mutation={SIGNUP_MUTATION} 
+          variables={{
+            googleId: id,
+            email: email,
+            firstName: firstName,
+            lastName: lastName
+          }}
+          onCompleted={
+            (data) => {
+              const {success, message, token} = data.signUpWithGoogle
+              if (success) {
+                // TODO : make session work before redirecting
+
+                this.props.history.push('/')
+              } else {
+                window.alert('Signup failed ... please contact admin')
+                this.props.history.push('/signup')
+              }
+            }
+          }
+        >
+          {(signupMutation) => {
+            return (<button onClick={() => {
+              // TODO : input validation (not for v0)
+              signupMutation();
+            }
+            }>Register</button>)
+          }}
+        </Mutation>
+        {/* A google log in button component */}
         <GoogleLogin
           clientId={googleAuthAPIClientID}
           buttonText="Google Log-in"
           onSuccess={this.responseGoogle}
           onFailure={this.responseFail}
         />
+
       </div>
     );
   }
@@ -125,4 +115,5 @@ class SignupForm extends Component {
 
 SignupForm.propTypes = {};
 
-export default SignupForm;
+// wrap with withRouter to use props.history
+export default withRouter(SignupForm);
--- web/src/pages/signup.js
+++ web/src/pages/signup.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import SignUpForm from "../components/signup-form";
+
 class Signup extends Component {
   render() {
     return (
-      <div>
-        <SignUpForm client={this.props.client}/>
-      </div>
+      <SignUpForm />
     );
   }
 }
