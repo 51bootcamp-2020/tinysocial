9aa2d2eecc74c81ba3a29d663d660a0b3d1c9cac
Myounghee96 <tjaudgml59@gmail.com>
2020-01-15 23:24:06 -0800

    refactor: remove featuredCard in eventlist page and change the card component from bootstrap to material-ui

--- web/src/pages/eventslist.js
+++ web/src/pages/eventslist.js
@@ -1,38 +1,10 @@
 import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Cards from "../components/eventslist-component/cards";
-import Paper from "@material-ui/core/Paper";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
 
-import PropTypes from 'prop-types';
-import Grid from "@material-ui/core/Grid";
+import {Container, Row, Col, Tabs, Tab} from 'react-bootstrap';
 
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-      <Typography
-          component="div"
-          role="tabpanel"
-          hidden={value !== index}
-          id={`scrollable-auto-tabpanel-${index}`}
-          aria-labelledby={`scrollable-auto-tab-${index}`}
-          {...other}
-      >
-        {/* if value == index, return the children(cards component) for each panel */}
-        {value === index && <Box p={3}>{children}</Box>}
-      </Typography>
-  );
-}
-// define the type of prop of TabPanel
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
 
 class Eventslist extends Component {
   constructor(props) {
@@ -46,76 +18,35 @@ class Eventslist extends Component {
     ];
 
     this.state = {
-      places: places,
-      selectedTap: 0
+      places: places
     };
   }
 
-  // each tab component of of event places
   TabComponents = () => {
     const components = [];
 
     for(let j = 0; j < this.state.places.length; j++){
-      components.push(<Tab label={this.state.places[j].title} />);
-    }
-
-    return components;
-  };
-
-  // panel(filter, cards) of selected tab
-  TabPanelComponents = () => {
-    const components = [];
-
-    // match the aligned cards to each place panel
-    for(let j = 0; j < this.state.places.length; j++){
-      components.push(<TabPanel value={this.state.selectedTap} index={j}>
+      components.push(<Tab eventKey={this.state.places[j].eventKey} title={this.state.places[j].title} class="center-block">
         <Cards area={this.state.places[j].eventKey}/>
-      </TabPanel>);
+      </Tab>);
     }
 
     return components;
   };
 
-  // update the selectedTab value to new selected value of tabs
-  handleChange = (event, newValue) => {
-    this.setState({
-      selectedTap: newValue
-    });
-  }
-
-  // eventslist render
   render() {
     return (
         <div>
-          <Grid
-              container
-              spacing={0}
-              direction="column"
-              alignItems="center"
-              justify="center"
-              style={{ minHeight: '100vh' }}
-          >
-
-            <Grid item xs={8}>
-              <Paper className={{flexGrow: 1}}>
-                <Tabs
-                    value={this.state.selectedTap}
-                    onChange={this.handleChange}
-                    indicatorColor="primary"
-                    textColor="primary"
-                >
-                  {/* show tab components which can select place of events */}
+          <br/>
+          <Container>
+            <Row className="justify-content-md-center">
+              <Col md="auto">
+                <Tabs defaultActiveKey={this.state.places[0].eventKey}>
                   {this.TabComponents()}
                 </Tabs>
-
-                {/* show tab panel components which show cards list by selected place tab */}
-                {this.TabPanelComponents()}
-              </Paper>
-            </Grid>
-
-          </Grid>
-          <br/>
-
+              </Col>
+            </Row>
+          </Container>
         </div>
     )
   }
