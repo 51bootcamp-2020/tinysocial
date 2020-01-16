9c1cd963bef41cfe80108e872b32672aec9fad9e
Sang-geon Yun <c0510gy@protonmail.ch>
2020-01-16 11:52:23 -0800

    refactor: change tab component from bootstrap to material-ui

--- web/src/index.js
+++ web/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './service-worker';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 
