4eba3867a78d311bc057f153a356a4466578a259
Sang-geon Yun <c0510gy@protonmail.ch>
2020-01-16 12:06:15 -0800

    feat: make components are located in center

--- web/src/pages/eventslist.js
+++ web/src/pages/eventslist.js
@@ -8,6 +8,7 @@ import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
 
 import PropTypes from 'prop-types';
+import Grid from "@material-ui/core/Grid";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -21,11 +22,12 @@ function TabPanel(props) {
           aria-labelledby={`scrollable-auto-tab-${index}`}
           {...other}
       >
+        {/* if value == index, return the children(cards component) for each panel */}
         {value === index && <Box p={3}>{children}</Box>}
       </Typography>
   );
 }
-
+// define the type of prop of TabPanel
 TabPanel.propTypes = {
   children: PropTypes.node,
   index: PropTypes.any.isRequired,
@@ -49,6 +51,7 @@ class Eventslist extends Component {
     };
   }
 
+  // each tab component of of event places
   TabComponents = () => {
     const components = [];
 
@@ -59,9 +62,11 @@ class Eventslist extends Component {
     return components;
   };
 
+  // panel(filter, cards) of selected tab
   TabPanelComponents = () => {
     const components = [];
 
+    // match the aligned cards to each place panel
     for(let j = 0; j < this.state.places.length; j++){
       components.push(<TabPanel value={this.state.selectedTap} index={j}>
         <Cards area={this.state.places[j].eventKey}/>
@@ -71,38 +76,46 @@ class Eventslist extends Component {
     return components;
   };
 
+  // update the selectedTab value to new selected value of tabs
   handleChange = (event, newValue) => {
     this.setState({
       selectedTap: newValue
     });
   }
 
+  // eventslist render
   render() {
     return (
         <div>
+          <Grid
+              container
+              spacing={0}
+              direction="column"
+              alignItems="center"
+              justify="center"
+              style={{ minHeight: '100vh' }}
+          >
+
+            <Grid item xs={8}>
+              <Paper className={{flexGrow: 1}}>
+                <Tabs
+                    value={this.state.selectedTap}
+                    onChange={this.handleChange}
+                    indicatorColor="primary"
+                    textColor="primary"
+                >
+                  {/* show tab components which can select place of events */}
+                  {this.TabComponents()}
+                </Tabs>
+
+                {/* show tab panel components which show cards list by selected place tab */}
+                {this.TabPanelComponents()}
+              </Paper>
+            </Grid>
+
+          </Grid>
           <br/>
-          <Paper className={{flexGrow: 1}}>
-            <Tabs
-                value={this.state.selectedTap}
-                onChange={this.handleChange}
-                indicatorColor="primary"
-                textColor="primary"
-            >
-              {this.TabComponents()}
-            </Tabs>
-            {this.TabPanelComponents()}
-          </Paper>
-          {/*
-                <Container>
-                    <Row className="justify-content-md-center">
-                        <Col md="auto">
-                            <Tabs defaultActiveKey={this.state.places[0].eventKey}>
-                                {this.TabComponents()}
-                            </Tabs>
-                        </Col>
-                    </Row>
-                </Container>
-                */}
+
         </div>
     )
   }
