dc80dce805dcb4feefbdca3e1bae579f9ad288c5
YoonYeoHwan <johnyhgmg@kookmin.ac.kr>
2020-01-17 16:47:10 -0800

    feat(navigation bar): update navigation bar to responsive UI

--- web/src/NavBar.js
+++ web/src/NavBar.js
@@ -1,60 +1,20 @@
 import {
-  AppBar,
-  Grid,
-  Tabs,
-  Tab,
+  isWidthUp,
+  withWidth,
 } from '@material-ui/core';
-import {
-  BrowserRouter,
-  Link,
-  Route,
-  Switch,
-} from 'react-router-dom';
-import Events from './pages/eventlist';
-import Landing from './pages/landing';
-import Logo from './img/tinysocial-logo.png';
+import NavBarPC from './NavBarPC';
+import NavBarMobile from './NavBarMobile';
 import React from 'react';
-import Signin from './pages/signin';
 
-function Navbar() {
+function Navbar(props) {
+  if (isWidthUp('md', props.width)) {
+    return (
+        <NavBarPC/>
+    );
+  }
   return (
-      <BrowserRouter>
-
-        <div className="Navbar">
-          <Route
-              path="/"
-              render={({location}) => (
-                  <>
-                    {/* TODO : need to make it flex(different UI between mobile web and PC web) */}
-                    <AppBar color="default">
-                      <Grid container direction="row" justify="space-between" alignItems="center">
-                        <Grid justify="flex-start">
-                          <a href="/" style={{ padding : 20 }}>
-                            <img src={Logo} width="130" height="18"/>
-                          </a>
-                        </Grid>
-                        <Grid justify="flex-end">
-                          <Tabs value={location.pathname}
-                                aria-label="Navigation Tabs">
-                            <Tab label="Events" component={Link} to="/"/>
-                            {/* TODO : need to implement About page. Now just linked to landing page */}
-                            <Tab label="About" component={Link} to="/landing"/>
-                            <Tab label="Sign in" component={Link} to="/signin"/>
-                          </Tabs>
-                        </Grid>
-                      </Grid>
-                    </AppBar>
-                    <Switch>
-                      <Route path='/landing' render={() => <Landing/>}/>
-                      <Route path="/signin" render={() => <Signin/>}/>
-                      <Route path='/' render={() => <Events/>}/>
-                    </Switch>
-                  </>
-              )}
-          />
-        </div>
-      </BrowserRouter>
+      <NavBarMobile/>
   );
 }
 
-export default Navbar;
+export default withWidth()(Navbar);
