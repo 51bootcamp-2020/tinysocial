9d4c18493f9ebe050297eb6f072e950ce8c2584c
SeongJaeSong <tjdkskgnal61@gmail.com>
2020-01-19 14:13:09 -0800

    feat: Add getUserUpcomingEvent API

--- server/src/resolvers/query.js
+++ server/src/resolvers/query.js
@@ -1,10 +1,20 @@
 module.exports.Query = {
-  events: async (_, {pageSize, after}) => {
+  events: async (_, { pageSize, after }) => {},
+  event: async (_, { id }) => {},
+  me: async (_, __, context) => {},
+  user: async (_, { id }) => {},
+  upcomingEvents: async (_, { userId }, { dataSources }) => {
+    const events = await dataSources.mainAPI.getUserUpcomingEvents({ userId });
+    if (events === undefined) {
+      return null;
+    }
+    return events;
   },
-  event: async (_, {id}) => {
-  },
-  me: async (_, __, context) => {
-  },
-  user: async (_, {id}) => {
-  },
+  pastEvents: async (_, { userId }, { dataSources }) => {
+    const events = await dataSources.mainAPI.getUserPastEvents(userId);
+    if (events === undefined) {
+      return null;
+    }
+    return events;
+  }
 };
