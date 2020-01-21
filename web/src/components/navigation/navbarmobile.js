eaf3b3050d2343f363572a451a4eeadd511d5b15
YoonYeoHwan <johnyhgmg@kookmin.ac.kr>
2020-01-20 23:00:42 -0800

    fix: Apply code reviews

--- web/src/components/navigation/navbarmobile.js
+++ web/src/components/navigation/navbarmobile.js
@@ -5,7 +5,7 @@ import {
   Drawer,
   Grid,
   IconButton,
-  List, Tab,
+  List,
   Toolbar,
   Typography,
 } from '@material-ui/core';
@@ -45,7 +45,7 @@ function NavBarMobile() {
         <Divider/>
         <List>
           <Grid container
-                direction="column">
+              direction="column">
             <a href="/" className={classes.listItem}>
               <Typography variant="button" style={{textTransform: 'none'}}>
                 Events
