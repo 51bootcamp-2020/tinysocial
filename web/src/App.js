b6116a5eab5c76c191f0e37a46e3977dcc9112bd
mskwon1 <mskwon@kookmin.ac.kr>
2020-01-23 11:02:49 -0800

    fix: duplicate ApolloProvider
    resolves #35

--- web/src/App.js
+++ web/src/App.js
@@ -1,36 +1,27 @@
-import ApolloClient from "apollo-boost";
-import {ApolloProvider} from "react-apollo";
 import {
   BrowserRouter as Router,
   Route,
   Switch,
-} from "react-router-dom"
+} from "react-router-dom";
 import React from "react";
-import Signin from "./pages/signin"
+import Signin from "./pages/signin";
 import Signup from "./pages/signup";
-
-const client = new ApolloClient({
-  //TODO(arin-kwak): need update uri.
-  uri: "http://localhost:15780"
-});
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <Router>
-        <Switch>
-          <Route path="/signup">
-            <Signup/>
-          </Route>
-          <Route path="/signin">
-            <Signin/>
-          </Route>
-          <Route path="/">
-            Main
-          </Route>
-        </Switch>
-      </Router>
-    </ApolloProvider>
+    <Router>
+      <Switch>
+        <Route path="/signup">
+          <Signup/>
+        </Route>
+        <Route path="/signin">
+          <Signin/>
+        </Route>
+        <Route path="/">
+          Main
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
