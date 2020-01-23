28a2e6f77acccd578491c0c62477624be19f59d4
YoonYeoHwan <johnyhgmg@kookmin.ac.kr>
2020-01-22 23:55:10 -0800

    feat(eventdetail page): Implement event detail page

---
+++ web/src/pages/eventdetail.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import CommonEvent from '../components/eventdetail/commonevent';
+import BookClub from '../components/eventdetail/bookclub';
+import Ticket from '../components/eventdetail/ticket';
+import {
+  Hidden,
+  Divider,
+} from '@material-ui/core';
+
+function Eventdetail() {
+  return (
+      <div>
+        <CommonEvent/>
+        <BookClub/>
+        <Hidden smUp>
+          <Divider/>
+          <Ticket/>
+        </Hidden>
+      </div>
+  );
+}
+
+export default Eventdetail;
