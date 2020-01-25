80dca5683e9bb95207d165b48eb3871fdba54ffe
Hyejin Lee <hub0913@kookmin.ac.kr>
2020-01-24 16:35:02 -0800

    fix: App.js merge error resolve

--- web/src/App.js
+++ web/src/App.js
@@ -1,38 +1,36 @@
-import ApolloClient from "apollo-boost";
-import {ApolloProvider} from "react-apollo";
+import About from './pages/about';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
 import {
   BrowserRouter as Router,
   Route,
-  Switch
+  Switch,
 } from "react-router-dom"
-import React from "react";
-
-import Landing from './pages/landing'
-import Signin from "./pages/signin"
-import Signup from "./pages/signup";
+import EventList from './pages/eventlist';
+import Landing from './pages/landing';
+import NavBar from './components/navigation/navbar';
+import React from 'react';
+import Signin from './pages/signin';
 
 const client = new ApolloClient({
-  //TODO(arin-kwak): need update uri
-  uri: "http://localhost:15780"
+  //TODO(arin-kwak): need update uri.
+  uri: 'http://localhost:15780',
 });
 
 function App() {
+  {/* TODO(Myoung-heeSeo) : Add a state to check if the user is logged in now. */}
   return (
-    <ApolloProvider client={client}>
-      <Router>
-        <Switch>
-          <Route path="/signup">
-            <Signup/>
-          </Route>
-          <Route path="/signin">
-            <Signin/>
-          </Route>
-          <Route path="/">
-            <Landing/>
-          </Route>
-        </Switch>
-      </Router>
-    </ApolloProvider>
+      <ApolloProvider client={client}>
+        <NavBar/>
+        <Router>
+          <Switch>
+            <Route path='/signin' render={() => <Signin/>}/>
+            <Route path='/eventlist' render={() => <EventList/>}/>
+            <Route path='/about' render={() => <About/>}/>
+            <Route path='/' render={() => <Landing/>}/>
+          </Switch>
+        </Router>
+      </ApolloProvider>
   );
 }
 
