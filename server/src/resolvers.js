7a348a7b2110b251753fc82b93f167649b44e5ef
Arin Kwak <rev1c0sm0s@gmail.com>
2020-01-27 03:41:53 -0800

    fix: Add comments

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -12,6 +12,12 @@ const {EventSchedule} = require('./resolvers/eventSchedule');
 const {TagConnection} = require('./resolvers/tagConnection');
 const {EventConnection} = require('./resolvers/eventConnection');
 
+// Why should you write field-level resolvers fetching data manually instead of
+// default resolvers? Look at this references.
+// https://medium.com/paypal-engineering/graphql-resolvers-best-practices-cd36fdbcef55
+// https://www.apollographql.com/docs/apollo-server/data/data-sources/#batching
+// https://graphql.org/learn/best-practices/#server-side-batching-caching
+
 module.exports = {
   Query,
   Mutation,
