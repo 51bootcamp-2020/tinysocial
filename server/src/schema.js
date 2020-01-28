8ce9ff3b6b50337499a2491bfdc5adbd0c98469e
SiHyun Lee <lsh9034@kookmin.ac.kr>
2020-01-28 13:04:56 -0800

    fix: schema abort

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,11 +1,15 @@
 const {gql} = require('apollo-server');
 
 const typeDefs = gql`
-    scalar Date
     scalar DateTime
 
     type Query {
-        events(pageSize: Int, after: String): EventConnection!
+        # Returns certain size of events after the cursor
+        # Reference:
+        #  https://www.apollographql.com/docs/tutorial/resolvers/#paginated-queries 
+        #pageSize must be under 50 or same. If you don't send after, after is 0.
+        events(pageSize: Int, after: Int,
+            eventFilter: EventFilter, eventSort: EventSort): EventConnection!
         # Return specific event whose id is 'id'.
         # If not exist, return null
         event(id: ID!): Event
@@ -14,8 +18,19 @@ const typeDefs = gql`
         # Return the user whose id is 'id'.
         # If not exist, return null
         user(id: ID!): User
+        myEvents(info: String!): [Event]
+        userReviews(userId: Int, eventId: Int): [Review]
+        #if you don't send after, after is 0.
+        tagNames(pageSize: Int, after: Int): TagConnection!
     }
-
+    type Review {
+        eventId: ID!
+        userId: ID!
+        title: String!
+        content: String!
+        author: User!
+        isPublic: Boolean!
+    }
     type Mutation {
         signInWithGoogle(googleId: String!): AuthResponse!
         signUpWithGoogle(
@@ -25,18 +40,54 @@ const typeDefs = gql`
             lastName: String!
             profileImgUrl: String
         ): AuthResponse!
-        # If successed, then return True.  
-        logout: Boolean!
+        # Default Sign Up / Sign In
+        signUp(
+            email: String!
+            firstName: String!
+            lastName: String!
+            pw: String!
+            repw: String!
+        ): AuthResponse!
+        signIn(
+            email: String!
+            pw: String!
+        ): AuthResponse!
+        # Email Vlidation
+        emailValidate(
+            token: String!
+        ): AuthResponse!
+        # If successful, then return True.  
+        logOut: Boolean!
+        createOrModifyReview(
+            eventId: Int!
+            title: String!
+            content: String!
+            isPublic: Boolean!
+        ): Review!
+    }
+
+    input EventFilter {
+        recommendation: Boolean, # User-based recommendation flag
+        tagIds: [Int!]
+        # TODO(yun-kwak): range-based search
+        # range: Float,
+        # from: String 
+    }
+
+    enum EventSort {
+        BEST_MATCH,
+        NEWEST,
+        MOST_MEMBERS,
+        TIME_CLOSEST,
     }
 
     type AuthResponse {
         success: Boolean!
-        # Contains error message, if not successed
+        # Contains error message, if not successful
         message: String
-        # If not successed, this fields are null. 
+        # If not successful, these fields are null. 
         token: String
         user: User
-    
     }
 
     type User {
@@ -44,60 +95,93 @@ const typeDefs = gql`
         firstName: String!
         lastName: String!
         email: String!
-        age: Int
-        city: String
-        state: String
+        age: Int # TODO(yun-kwak): Calculate the age of the user from birthday
+        # TODO(yun-kwak): Split the address into city, state, zip, street,
+        # additional street addres
+        address: String
         phone: String
-        hostedEvents: [Event]!
-        participatedEvents: [Event]!
-        birthday: Date
-        createdAt: DateTime!
+        selfDescription: String
+        hostedEvents: [Event]! # Events hosted by this user.
+        participatedEvents: [Event]! # Events participated by this user.
+        birthday: DateTime
+        registrationDate: DateTime!
         profileImgUrl: String
+        lastInterationTime: DateTime
     }
 
-    type Event {
+    interface Event {
         id: ID!
         host: User!
-        createdAt: DateTime!
-        updatedAt: DateTime!
+        thumbnailUrl: String
+        creationTime: DateTime!
+        # When the schedule of the event is updated, lastUpdatedTime of the
+        # event also need to be updated.
+        lastUpdatedTime: DateTime!
         schedule: [EventSchedule]!
         title: String!
         description: String!
         price: Float!
-        # image: Upload!
         # TODO(arin-kwak): Implement image uploading feature
-        maxParticipants: Int!
+        # image: Upload!
         tags: [Tag]!
         participants: [User]!
+        maxParticipantNum: Int
+        reviews: [Review]
     }
 
-    # Every event can have multiple tags.
-    # Tags are predefined by ours(developers)
-    # and used by the event host to categorize his event.
-    # So we are able to categorize events by tags.
+    type EventBookClub implements Event{
+        id: ID!
+        host: User!
+        thumbnailUrl: String
+        creationTime: DateTime!
+        # When the schedule of the event is updated,
+        # lastUpdatedTime of the event also need to be updated.
+        lastUpdatedTime: DateTime!
+        schedule: [EventSchedule]!
+        title: String!
+        description: String!
+        price: Float!
+        bookImageUrl: String!
+        bookTitle: String!
+        bookAuthor: String!
+        bookDescription: String!
+        bookISBN: Int!
+        tags: [Tag]!
+        participants: [User]!
+        maxParticipantNum: Int
+        reviews: [Review]
+    }
+
+    # Every event can have multiple tags. Tags are predefined by ours(developers)
+    # and used by the event host to categorize his event. So we are able to
+    # categorize events by tags.
     type Tag {
         id: ID!
         name: String!
         events: [Event]!
     }
+    type TagConnection {
+        cursor: Int!
+        tags: [Tag]!
+    }
 
-    # TODO(arin-kwak): Implement EventConnection.
-    #  Reference:
-    #  https://www.apollographql.com/docs/tutorial/resolvers/#paginated-queries 
+    # TODO(lsh9034): Implement EventConnection.
+    # Reference:
+    # https://www.apollographql.com/docs/tutorial/resolvers/#paginated-queries 
     type EventConnection {
-        cursor: String!
-        hasMore: Boolean!
+        cursor: Int!
         events: [Event]!
     }
 
     type EventSchedule {
         id: ID!
-        start: DateTime!
-        end: DateTime!
-        locationLatitude: Float!
-        locationLongitude: Float!
-        city: String!
-        state: String!
+        startDateTime: DateTime!
+        endDateTime: DateTime!
+        # TODO(yun-kwak): Split the address into country, state, city, zip, 
+        # street, additionalStreetAdress
+        address: String!
+        latitude: Float!
+        longitude: Float!
     }
 `;
 
