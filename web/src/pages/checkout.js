bc10b7c694052db4d14d03634c6772d0e8c1b09f
Sang-geon Yun <c0510gy@protonmail.ch>
2020-01-27 23:55:39 -0800

    refactor: refactoring checkout.js

--- web/src/pages/checkout.js
+++ web/src/pages/checkout.js
@@ -1,28 +1,8 @@
 import React, {Component} from 'react';
 import PaypalPayment from '../components/paypal-payment';
 import PurchaseEventItem from '../components/purchase-event-item';
-import {Container, Grid, Paper, Divider, ButtonBase, Typography} from '@material-ui/core';
+import {Container, Divider} from '@material-ui/core';
 
-const classes = {
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: 10,
-    margin: 'auto',
-    maxWidth: 500,
-  },
-  image: {
-    width: 128,
-    height: 128,
-  },
-  img: {
-    margin: 'auto',
-    display: 'block',
-    maxWidth: '100%',
-    maxHeight: '100%',
-  },
-};
 
 class Checkout extends Component {
   render() {
@@ -30,7 +10,10 @@ class Checkout extends Component {
       <Container maxWidth='sm'>
         <br/>
 
-        <PurchaseEventItem price={'0.01'} eventName={'Brief summary of Human History'} schedule={'January 29, 2020 10:30 AM - 1:30 PM'} imageUrl={'./images/tmp_book.png'} />
+        <PurchaseEventItem price={'0.01'}
+          eventName={'Brief summary of Human History'}
+          schedule={'January 29, 2020 10:30 AM - 1:30 PM'}
+          imageUrl={'./images/tmp_book.png'} />
 
         <br/>
         <Divider />
