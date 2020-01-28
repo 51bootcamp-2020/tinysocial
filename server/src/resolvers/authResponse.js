08d9c1530d10f2bfe7f341a8ce6a0e7f971d24cf
SiHyun Lee <lsh9034@kookmin.ac.kr>
2020-01-27 18:25:07 -0800

    make authResponse resolver

--- server/src/resolvers/authResponse.js
+++ server/src/resolvers/authResponse.js
@@ -1,4 +1,15 @@
 module.exports.AuthResponse = {
-
+  success: async ({success}) => {
+    return success;
+  },
+  message: async ({message}) => {
+    return message;
+  },
+  token: async ({token}) => {
+    return token;
+  },
+  user: async ({id}) => {
+    return {id};
+  },
 };
 
