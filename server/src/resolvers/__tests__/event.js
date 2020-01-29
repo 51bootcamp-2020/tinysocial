3f93d6d7b102a1581363ce121e1461fb5a314636
SeongJaeSong <tjdkskgnal61@gmail.com>
2020-01-29 01:25:45 -0800

    refactor: add event resolver unit test

--- server/src/resolvers/__tests__/event.js
+++ server/src/resolvers/__tests__/event.js
@@ -1,2 +1,16 @@
-const resolvers = require('../../resolvers');
+const {Event} = require('../../resolvers');
 const {mockContext} = require('../mockContext');
+
+const {getTypeOfEvent} = mockContext.dataSources.eventAPI;
+
+describe('[EventResolver]', () => {
+  test('returns \'EventBookClub\'', async () => {
+    getTypeOfEvent.mockReturnValueOnce(0);
+    const res = await Event.__resolveType({id: 1}, mockContext);
+    expect(res).toBe('EventBookClub');
+  })
+  test('throws error', async () => {
+    getTypeOfEvent.mockReturnValueOnce(999);    
+    expect(Event.__resolveType({id: 1}, mockContext)).rejects.toThrow('Data cannot be resolved to any Event type. Please contact to backend developers.');
+  })
+})
