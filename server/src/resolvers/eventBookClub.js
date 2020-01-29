15ee9961ad639d443515a0b2d1f9b4f8e1bcf6fb
SiHyun Lee <lsh9034@kookmin.ac.kr>
2020-01-28 18:07:22 -0800

    fix: integrate type of get attribute function

--- server/src/resolvers/eventBookClub.js
+++ server/src/resolvers/eventBookClub.js
@@ -1,18 +1,18 @@
 module.exports.EventBookClub = {
   host: async ({id}, _, {dataSources}) => {
-    const hostId = dataSources.eventAPI.getHostIdOfEvent({eventId: id});
+    const hostId = dataSources.eventAPI.getAttributeOfEvent('hostId', id);
     return hostId;
   },
   thumbnailUrl: async ({id}, _, {dataSources}) => {
-    const thumbnailUrl = dataSources.eventAPI.getThumbnailUrlOfEvent(id);
+    const thumbnailUrl = dataSources.eventAPI.getAttributeOfEvent('thumbnailUrl', id);
     return thumbnailUrl;
   },
   creationTime: async ({id}, _, {dataSources}) => {
-    const creationTime = dataSources.eventAPI.getCreationTimeOfEvent(id);
+    const creationTime = dataSources.eventAPI.getAttributeOfEvent('creationTime', id);
     return creationTime;
   },
   lastUpdatedTime: async ({id}, _, {dataSources}) => {
-    const lastUpdatedTime = dataSources.eventAPI.getLastUpdatedTime(id);
+    const lastUpdatedTime = dataSources.eventAPI.getAttributeOfEvent('lastUpdatedTime', id);
     return lastUpdatedTime;
   },
   schedule: async ({id}, _, {dataSources}) => {
@@ -20,35 +20,35 @@ module.exports.EventBookClub = {
     return scheduleIds;
   },
   title: async ({id}, _, {dataSources}) => {
-    const title = dataSources.eventAPI.getTitleOfEvent(id);
+    const title = dataSources.eventAPI.getAttributeOfEvent('title', id);
     return title;
   },
   description: async ({id}, _, {dataSources}) => {
-    const description = dataSources.eventAPI.getDescriptionOfEvent(id);
+    const description = dataSources.eventAPI.getAttributeOfEvent('description', id);
     return description;
   },
   price: async ({id}, _, {dataSources}) => {
-    const price = dataSources.eventAPI.getPriceOfEvent(id);
+    const price = dataSources.eventAPI.getAttributeOfEvent('price', id);
     return price;
   },
   bookImageUrl: async ({id}, _, {dataSources}) => {
-    const bookImageUrl = dataSources.eventAPI.getBookImageUrlOfEvent(id);
+    const bookImageUrl = dataSources.eventAPI.getAttributeOfEvent('bookImageUrl', id);
     return bookImageUrl;
   },
   bookTitle: async ({id}, _, {dataSources}) => {
-    const bookTitle = dataSources.eventAPI.getBookTitleOfEvent(id);
+    const bookTitle = dataSources.eventAPI.getAttributeOfEvent('bookTitle', id);
     return bookTitle;
   },
   bookAuthor: async ({id}, _, {dataSources}) => {
-    const bookAuthor = dataSources.eventAPI.getBookAuthorOfEvent(id);
+    const bookAuthor = dataSources.eventAPI.getAttributeOfEvent('bookAuthor', id);
     return bookAuthor;
   },
   bookDescription: async ({id}, _, {dataSources}) => {
-    const bookDescription = dataSources.eventAPI.getBookDescriptionOfEvent(id);
+    const bookDescription = dataSources.eventAPI.getAttributeOfEvent('bookDescription', id);
     return bookDescription;
   },
   bookISBN: async ({id}, _, {dataSources}) => {
-    const bookISBN = dataSources.eventAPI.getBookISBNOfEvent(id);
+    const bookISBN = dataSources.eventAPI.getAttributeOfEvent('bookISBN', id);
     return bookISBN;
   },
 
@@ -61,7 +61,7 @@ module.exports.EventBookClub = {
     return participantIds;
   },
   maxParticipantNum: async ({id}, _, {dataSources}) => {
-    const maxParticipantNum = dataSources.eventAPI.getMaxParticipantNumOfEvent(id);
+    const maxParticipantNum = dataSources.eventAPI.getAttributeOfEvent('maxParticipantNum', id);
     return maxParticipantNum;
   },
   reviews: async ({id}, {userIds}, {userId}) =>{
