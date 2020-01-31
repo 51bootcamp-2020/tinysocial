f47b26975a3cfd6ae839510865e75674a3e7d060
Arin Kwak <rev1c0sm0s@gmail.com>
2020-01-30 22:31:54 -0800

    refactor: Add config file

---
+++ server/config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config({
+  path: require('path').
+      resolve(process.cwd(), 'src/.env'),
+});
+
+module.exports = {
+  'dev': {
+    'username': 'arin_kwak',
+    'password': process.env.DB_DEV_PASSWORD,
+    'database': process.env.DB_DEV_NAME,
+    'host': process.env.DB_DEV_HOST,
+    'dialect': 'mariadb',
+  },
+  'test': {
+    'dialect': 'sqlite',
+    'storage': 'database.sqlite',
+  },
+  'production': {
+    'username': 'yun_kwak',
+    'password': process.env.DB_PASSWORD,
+    'database': process.env.DB_NAME,
+    'host': process.env.DB_HOST,
+    'dialect': 'mariadb',
+  },
+};
