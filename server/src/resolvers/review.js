788933efdbd09f0e38386aed8d9211c0aebc84aa
mskwon1 <mskwon@kookmin.ac.kr>
2020-01-30 19:10:24 -0800

    fix: rollback server codes

--- server/src/resolvers/review.js
+++
@@ -1,6 +0,0 @@
-module.exports.Review = {
-  author: async (parent, __, {dataSources}) => {
-    const author = await dataSources.mainAPI.getAuthorFromReview(parent.userId);
-    return author;
-  },
-};
