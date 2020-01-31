a1306e7df9c402c017ccfec8106708a4f026177d
YoonYeoHwan <johnyhgmg@kookmin.ac.kr>
2020-01-31 15:47:13 -0800

    fix: Fix server url & fix date type error

--- web/src/index.js
+++ web/src/index.js
@@ -14,6 +14,7 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './service-worker';
 
 const httpLink = new HttpLink({
+  // TODO(arin-kwak): change this after deploying.
   uri: 'http://ec2-34-223-231-126.us-west-2.compute.amazonaws.com:15780/',
 });
 
