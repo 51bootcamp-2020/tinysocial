352df6db220d7870eab002267b43312fc9c46fac
Sang-geon Yun <c0510gy@protonmail.ch>
2020-02-01 10:55:09 -0800

    fix: remove test resolver from web side index.js

--- web/src/index.js
+++ web/src/index.js
@@ -31,7 +31,6 @@ const authMiddleware = new ApolloLink((operation, forward) => {
 const client = new ApolloClient({
   link: concat(authMiddleware, httpLink),
   cache: new InMemoryCache,
-  resolvers,
 });
 
 ReactDOM.render(
