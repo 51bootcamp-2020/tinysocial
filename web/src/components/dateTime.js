576db158aea0b142869545281a8e187a5b0f13f1
YoonYeoHwan <johnyhgmg@kookmin.ac.kr>
2020-02-06 14:36:05 -0800

    fix: Apply code review

--- web/src/components/dateTime.js
+++ web/src/components/dateTime.js
@@ -10,18 +10,33 @@ class Datetime extends Component {
     };
   }
   render() {
-    const date = new Date(this.state.schedule.startDateTime);
-    const month = date.toLocaleString('US', {month: 'long'}).substring(0, 3);
-    const year = date.toLocaleString('US', {year: 'numeric'});
-    const day = date.toLocaleString('US', {day: '2-digit'});
+    const startDate = new Date(this.state.schedule.startDateTime);
+    const endDate = new Date(this.state.schedule.endDateTime);
+    const month = startDate.toLocaleString('US', {month: 'long'}).
+        substring(0, 3);
+    const day = startDate.toLocaleString('US', {day: 'numeric'});
     const options = {
       hour: '2-digit',
       minute: '2-digit',
     };
-    const time = date.toLocaleString('US', options);
+    const startTime = startDate.toLocaleString('US', options);
+    const endTime = endDate.toLocaleString('US', options);
+    let dateForm = '';
+    if (day >= 11 && day <= 13) {
+      dateForm = 'th';
+    } else {
+      switch (day % 10) {
+        case 1: dateForm = 'st';
+        case 2: dateForm = 'nd';
+        case 3: dateForm = 'rd';
+        default: dateForm = 'th';
+      }
+    }
+
     return (
+      // Feb 5th, 01:30 PM
       <Typography variant='body2' style={{marginBottom: 5}}>
-        {month} {day}, {year}, {time}
+        {month} {day}{dateForm}, {startTime} - {endTime}
       </Typography>
     );
   }
