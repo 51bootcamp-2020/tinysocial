154db44251ddfba69303149cbc644fe36c7daa31
YoonYeoHwan <johnyhgmg@kookmin.ac.kr>
2020-02-06 01:22:51 -0800

    feat(event detail page): merge event detail page

---
+++ web/src/components/eventdetail/schedules.js
@@ -0,0 +1,36 @@
+import {
+  Typography,
+} from '@material-ui/core';
+import PropTypes from 'prop-types';
+import React, {Component} from 'react';
+
+class Schedules extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const scheduleList = [];
+    let scheduleLength = Math.min(this.props.children.length, 3);
+    if (this.props.children.length > 3 && this.props.moreButton) {
+      scheduleLength = this.props.children.length;
+    }
+    for (let i = 0; i < scheduleLength; i++) {
+      const date = new Date(this.props.children[i].startDateTime);
+      const month = date.toLocaleString('default', {month: 'long'});
+      scheduleList.push(
+          <Typography variant='body2' style={{marginBottom: 5}}>
+            {month} {date.getDate()}, {date.getFullYear()}
+          </Typography>,
+      );
+    }
+    return <>{scheduleList}</>;
+  }
+}
+
+Schedules.propTypes = {
+  children: PropTypes.element.isRequired,
+  moreButton: PropTypes.bool.isRequired,
+};
+
+export default Schedules;
