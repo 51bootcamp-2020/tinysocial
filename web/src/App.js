e0a7b837f6a9da80a05ed5a34fb91eb7950b33db
SiHyun Lee <lsh9034@kookmin.ac.kr>
2020-02-06 17:56:59 -0800

    fix: import signout component & bug fix

--- web/src/App.js
+++ web/src/App.js
@@ -11,18 +11,20 @@ import NavBar from './components/navigation/navbar';
 import React from 'react';
 import SignInUp from './pages/signInUp';
 import UserPage from './pages/userpage';
+import SignOut from './pages/signout';
 
 function App() {
   return (
     <Router>
       <NavBar/>
       <Switch>
-        <Route path='/about' render={() => <About/>}/>
-        <Route path="/emailvalidation" render={() => <Emailvalidation/>}/>
-        <Route path='/eventlist' render={() => <EventList/>}/>
-        <Route path='/signin' render={() => <SignInUp/>}/>
-        <Route path='/userpage' render={() => <UserPage/>}/>
-        <Route path='/' render={() => <Landing/>}/>
+        <Route path='/about' render={() => <div><NavBar/><About/></div>}/>
+        <Route path="/emailvalidation" render={() => <div><NavBar/><Emailvalidation/></div>}/>
+        <Route path='/eventlist' render={() => <div><NavBar/><EventList/></div>}/>
+        <Route path='/signin' render={() => <div><NavBar/><SignInUp/></div>}/>
+        <Route path='/signout' render={() => <SignOut/>}/>
+        <Route path='/userpage' render={() => <div><NavBar/><UserPage/></div>}/>
+        <Route path='/' render={() => <div><NavBar/><Landing/></div>}/>
       </Switch>
     </Router>
   );
